1e32ef673c11eb6351d6850e450cd9bb134ec4b2
Anuj Verma <anuj.verma.20132@gmail.com>
2020-10-07 18:40:19 +0530

    Initialized repository

---
+++ QP, documentation and experimentation/Planning.md
@@ -0,0 +1,2 @@
+# 9608/42/PRE/O/N/20
+Last update: Anuj Verma, 03:16 PM 06/10/2020
---
+++ Standalone Compiled Program.py
@@ -0,0 +1,3 @@
+# # 9608/42/PRE/O/N/2020
+# # Standalone Compliled Program
+# # The code below is for every task combined into one, and can run independently.
